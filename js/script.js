347f19fc9da4168aea2489fb3877c562286453a5
Micaela Noriega <micaelanoriega04@gmail.com>
2025-12-05 18:58:51 +0100

    Viendo api

--- js/script.js
+++ js/script.js
@@ -1,49 +1,42 @@
-$(document).ready(function(){
-    // $.ajax({
-    // url: 'https://randomuser.me/api/',
-    // dataType: 'json',
-    // success: function(data) {
-    //     console.log(data);
-    // }
+$(document).ready(function () {
+  // $.ajax({
+  // url: 'https://randomuser.me/api/',
+  // dataType: 'json',
+  // success: function(data) {
+  //     console.log(data);
+  // }
 
+  const $container = $("#userCont");
 
-    fetch("https://randomuser.me/api")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(result){
-            console.log(result);
-            console.log(result.results[0].name.first);
-            console.log(result.results[0].name.last);
-            console.log(result.results[0].picture.large);
+  fetch("https://randomuser.me/api")
+    .then(function (response) {
+      if (response.ok) {
+        throw new Error("Usuarios error");
+      }
 
+      return response.json();
+    })
+    .then(function (result) {
+      console.log(result.results[0]);
 
-            // $("#user-cont").append(result.results[0].name.first);
-            // $("#user-cont").append(result.results[0].name.last);
-            // $("#user-cont").append('<img src="' + result.results[0].picture.large + '"alt="">');
-            // $("#user-cont").append(`<img src="${result.results[0].picture.medium} "alt="">`);
+      let user = result.results[0];
 
-            let fullName = result.results[0].name.first + " " + result.results[0].name.last;
-            let fullContact = result.results[0].cell + " " + result.results[0].email;
-            let fullLocation = result.results[0].location.city +", " + result.results[0].location.country;
-
-
-            // $("#uName").append(result.results[0].name.first);
-            // $("#uName").append(result.results[0].name.last);
-            $("#uName").append(fullName);
-
-            $("#uImg").append('<img src="' + result.results[0].picture.large + '"alt="">');
-
-            // $("#uLocation").append(result.results[0].location.city);
-            // $("#uLocation").append(result.results[0].location.country);
-            $("#uLocation").append(fullLocation);
-
-
-            // $("#uContact").append(result.results[0].cell);
-            // $("#uContact").append(result.results[0].email);
-            $("#uContact").append(fullContact);
-
-            
-        })
+      let card = `
+            <div> 
+                <img src=" ${user.picture.large}" alt="">
+                <div>
+                    <h3>${user.name.title} ${user.name.first} ${user.name.last}</h3>
+                    <p> ${user.location.city} -  ${user.location.country} </p>
+                    <div>
+                        <a href="mailto: ${user.email}">${user.email}</a>
+                        <span>${user.phone}</span>
+                    </div>    
+                </div>            
+             </div>
+            `;
+      $container.append(card);
+    })
+    .catch(function () {
+      $container.append("Perdoneme error");
+    });
 });
-
