aa643c1ae84ffe4dd9e938c23906ca3107d57516
Jaime <jaimeamayasanchez02@gmail.com>
2025-12-05 16:30:05 +0100

    datos api

--- js/script.js
+++ js/script.js
@@ -18,10 +18,32 @@ $(document).ready(function(){
             console.log(result.results[0].picture.large);
 
 
-            $("#user-cont").append(result.results[0].name.first);
-            $("#user-cont").append('<img src="' + result.results[0].picture.large + '"alt="">');
-            $("#user-cont").append(`<img src="${result.results[0].picture.medium} "alt="">`);
+            // $("#user-cont").append(result.results[0].name.first);
+            // $("#user-cont").append(result.results[0].name.last);
+            // $("#user-cont").append('<img src="' + result.results[0].picture.large + '"alt="">');
+            // $("#user-cont").append(`<img src="${result.results[0].picture.medium} "alt="">`);
+
+            let fullName = result.results[0].name.first + " " + result.results[0].name.last;
+            let fullContact = result.results[0].cell + " " + result.results[0].email;
+            let fullLocation = result.results[0].location.city +", " + result.results[0].location.country;
+
+
+            // $("#uName").append(result.results[0].name.first);
+            // $("#uName").append(result.results[0].name.last);
+            $("#uName").append(fullName);
+
+            $("#uImg").append('<img src="' + result.results[0].picture.large + '"alt="">');
+
+            // $("#uLocation").append(result.results[0].location.city);
+            // $("#uLocation").append(result.results[0].location.country);
+            $("#uLocation").append(fullLocation);
+
+
+            // $("#uContact").append(result.results[0].cell);
+            // $("#uContact").append(result.results[0].email);
+            $("#uContact").append(fullContact);
 
             
         })
 });
+
